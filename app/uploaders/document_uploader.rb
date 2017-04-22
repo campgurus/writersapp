33deb78dbe8f54089d20adcc773c4af36af3cb5b
Darius Goore <dariusgoore@Dariuss-MBP.fios-router.home>
2017-04-22 13:50:16 -0400

    editing doc format whitelist
    will only allow docx format for now.

--- app/uploaders/document_uploader.rb
+++ app/uploaders/document_uploader.rb
@@ -37,7 +37,7 @@ class DocumentUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
-    %w(pdf doc htm html docx)
+    %w(docx)
   end
 
   # Override the filename of the uploaded files:
