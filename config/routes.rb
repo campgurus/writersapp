d669738db81646ef062252f109e141320483b205
Darius Goore <dariusgoore@Dariuss-MBP.fios-router.home>
2017-04-29 16:08:32 -0400

    minor edits to comments/index
    also adding user_id to comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
+  
+  resources :users do
+    resources :registrations
+  end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'writings#index'
-  resources :writings
-  
+  resources :writings do
+  	resources :paragraphs
+  end
+
   resources :paragraphs do
 	resources :comments
   end
